2697f12045c89ab9e1ced0bdd54eeb5d99faf0ae
Steven.T <u3568545@connect.hku.hk>
2024-04-21 19:18:53 +0800

    Added coin sound effect and applied audio to all scene

--- COMP3329-Group17/Assets/Characters/Penguin/Scripts/CoinCollection.cs
+++ COMP3329-Group17/Assets/Characters/Penguin/Scripts/CoinCollection.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public class WinterCoin : MonoBehaviour
 {
+    public AudioClip coinSound;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player")) // Make sure your player GameObject has the tag "Player"
         {
             // Optionally, play a sound or animation
+            AudioSource.PlayClipAtPoint(coinSound, transform.position);
             // CoinManager.Instance.AddCoins(1); // Add 1 coin to the total count
             Destroy(gameObject); // Destroy the coin object
         }
